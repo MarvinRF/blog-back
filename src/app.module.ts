d7bb16faa5ded2e1d6d1f59cd61194a4c9f30f7e
Marvin Rocha <marvinvca@outlook.com>
2025-05-28 18:23:42 -0300

    Adds TypeORM integration with PostgreSQL
    Integrates database support by adding TypeORM, NestJS TypeORM, and related drivers. Updates configuration to enable auto-loading entities.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -12,6 +13,16 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     PostModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '5432'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true, //se cair em prod = 1:1 com RH
+      autoLoadEntities: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
