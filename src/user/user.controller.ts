e3bb442e8790e2d8490bf301628cc35a205722c3
Marvin Rocha <marvinvca@outlook.com>
2025-05-28 15:59:55 -0300

    Variaveis de ambiente, .env e ConfigModule (nestjs/config

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CustomParseIntPipe } from '../common/pipes/custom-parse-int-pipe.pipe';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get(':id')
   findOne(
     @Param('id', CustomParseIntPipe)
     id: number,
   ) {
+    console.log(process.env.TESTE);
     return {
       success: true,
       message: `This action returns a user with id: ${id}`,
