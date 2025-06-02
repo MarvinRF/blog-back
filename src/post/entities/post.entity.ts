639e553fd5a1c646ec1043b345094383cc15c391
Marvin Rocha <marvinvca@outlook.com>
2025-06-02 17:53:26 -0300

    Adds Post entity with complete field definitions
    Introduces a fully defined entity using ORM decorators with key attributes such as unique identifier, title, slug, content, excerpt, and timestamps.
    Lays groundwork for future relational mappings.

--- src/post/entities/post.entity.ts
+++ src/post/entities/post.entity.ts
@@ -1,1 +1,39 @@
-export class Post {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column({ unique: true })
+  slug: string;
+
+  @Column('text')
+  content: string;
+
+  @Column()
+  excerpt: string;
+
+  @Column({ nullable: true })
+  coverImageUrl: string;
+
+  @Column({ default: false })
+  isPublished: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  //many to One <- authorId <- FK para User <- muitos posts para 1 user
+}
