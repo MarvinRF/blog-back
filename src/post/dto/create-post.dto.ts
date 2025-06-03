04f6347e459f487b6ad7eff91d7f67054d0369aa
Marvin Rocha <marvinvca@outlook.com>
2025-06-03 10:12:46 -0300

    Enhances post DTO validation rules
    Adds detailed validations for post creation and update payloads.
    - Enforces string type, length, and non-empty validations for key post fields.
    - Restricts update properties using PickType and adds an optional published flag with boolean validation.

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,27 @@
-export class CreatePostDto {}
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  Length,
+} from 'class-validator';
+
+export class CreatePostDto {
+  @IsString({ message: 'title must be a string' })
+  @Length(10, 150, { message: 'title must have between 10 and 150 characters' })
+  title: string;
+
+  @IsString({ message: 'excerpt must be a string' })
+  @Length(10, 200, {
+    message: 'excerpt must have between 10 and 200 characters',
+  })
+  excerpt: string;
+
+  @IsString({ message: 'the main content must be a string' })
+  @IsNotEmpty({ message: 'content should not be empty' })
+  content: string;
+
+  @IsUrl({ require_tld: false }) //top level domain prohibits localhost e IP
+  @IsOptional()
+  coverImageUrl: string;
+}
--- src/post/dto/update-post.dto.ts
+++ src/post/dto/update-post.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 import { CreatePostDto } from './create-post.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdatePostDto extends PartialType(CreatePostDto) {}
+export class UpdatePostDto extends PartialType(
+  PickType(CreatePostDto, ['title', 'coverImageUrl', 'excerpt', 'content']),
+) {
+  @IsOptional() //vai depender da lógica que criarmos no service ou no Next.js
+  @IsBoolean({ message: 'publish post field must be a boolean' })
+  published?: boolean;
+}
