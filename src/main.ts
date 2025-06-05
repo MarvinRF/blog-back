e4f13a65208250dfad9295804e3eee6646d001c6
Marvin Rocha <marvinvca@outlook.com>
2025-06-05 17:39:40 -0300

    Add helmet middleware and update CORS config
    Incorporates security improvements by installing and applying a security headers middleware, which enforces a specific cross-origin resource policy. Adjusts CORS middleware to use a configurable whitelist and updates the environment port variable.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import { parseCorsWhiteList } from './common/utils/parse-cors-whitelist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(
+    helmet({
+      crossOriginResourcePolicy: { policy: 'cross-origin' },
+    }),
+  );
+
+  const CorsWhiteList = parseCorsWhiteList(process.env.CORS_WHITELIST ?? '');
+
+  app.enableCors({
+    origin: (
+      origin: string | undefined, // Isso é do navegador e para proteger o cliente
+      callback: (...args: any[]) => void,
+    ) => {
+      // Requisição sem origin ou que inclui uma origem conhecida
+      // por corsWhiteLists é permitida
+      if (!origin || CorsWhiteList.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +37,6 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.APP_PORT ?? 3001);
 }
 void bootstrap();
